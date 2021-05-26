9092c0784d3b58f0d7c3100c9cee20b1f06d404b
Nanqing Luo (Billy) <billyrobertluo@gmail.com>
2021-05-26 10:15:14 +0800

    Add files via upload

---
+++ Crawl/UserInfo.py
@@ -0,0 +1,7 @@
+class SecretSetting:
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+
+secret = SecretSetting("username", "password")
